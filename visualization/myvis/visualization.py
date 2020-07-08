38616877774e1ff50df95bcf42910dbcc0ed3d6f
Nikita Kasatkin <niksonkk44@gmail.com>
2020-07-08 13:21:42 +0300

    v1
    Supporting only two classes training

---
+++ visualization/myvis/visualization.py
@@ -0,0 +1,251 @@
+import os
+import sys
+import numpy as np
+import cv2
+import argparse
+
+import kitti_util as utils
+
+
+class kitti_object(object):
+    """Load and parse object data into a usable format."""
+
+    def __init__(self, root_dir, split="training", args=None):
+        """root_dir contains training and testing folders"""
+        self.root_dir = root_dir
+        self.split = split
+        print(root_dir, split)
+        self.split_dir = os.path.join(root_dir, split)
+
+        if split == "training":
+            self.num_samples = 7481
+        elif split == "testing":
+            self.num_samples = 7518
+        else:
+            print("Unknown split: %s" % (split))
+            exit(-1)
+
+        lidar_dir = "velodyne"
+        depth_dir = "depth"
+        if args is not None:
+            lidar_dir = args.lidar
+            depth_dir = args.depthdir
+
+        self.image_dir = os.path.join(self.split_dir, "image_2")
+        self.label_dir = os.path.join(self.split_dir, "label_2")
+        self.calib_dir = os.path.join(self.split_dir, "calib")
+
+        self.depthpc_dir = os.path.join(self.split_dir, "depth_pc")
+        self.lidar_dir = os.path.join(self.split_dir, lidar_dir)
+        self.depth_dir = os.path.join(self.split_dir, depth_dir)
+        self.pred_dir = args.preds_path
+
+    def __len__(self):
+        return self.num_samples
+
+    def get_image(self, idx):
+        assert idx < self.num_samples
+        img_filename = os.path.join(self.image_dir, "%06d.png" % (idx))
+        return utils.load_image(img_filename)
+
+    def get_lidar(self, idx, dtype=np.float64, n_vec=4):
+        assert idx < self.num_samples
+        lidar_filename = os.path.join(self.lidar_dir, "%06d.bin" % (idx))
+        print(lidar_filename)
+        return utils.load_velo_scan(lidar_filename, dtype, n_vec)
+
+    def get_calibration(self, idx):
+        assert idx < self.num_samples
+        calib_filename = os.path.join(self.calib_dir, "%06d.txt" % (idx))
+        return utils.Calibration(calib_filename)
+
+    def get_label_objects(self, idx):
+        assert idx < self.num_samples and self.split == "training"
+        label_filename = os.path.join(self.label_dir, "%06d.txt" % (idx))
+        return utils.read_label(label_filename)
+
+    def get_pred_objects(self, idx):
+        assert idx < self.num_samples
+        pred_filename = os.path.join(self.pred_dir, "%06d.txt" % (idx))
+        print(pred_filename)
+        is_exist = os.path.exists(pred_filename)
+        if is_exist:
+            return utils.read_label(pred_filename)
+        else:
+            return None
+
+    def get_depth(self, idx):
+        assert idx < self.num_samples
+        img_filename = os.path.join(self.depth_dir, "%06d.png" % (idx))
+        return utils.load_depth(img_filename)
+
+    def get_depth_image(self, idx):
+        assert idx < self.num_samples
+        img_filename = os.path.join(self.depth_dir, "%06d.png" % (idx))
+        return utils.load_depth(img_filename)
+
+    def get_depth_pc(self, idx):
+        assert idx < self.num_samples
+        lidar_filename = os.path.join(self.depthpc_dir, "%06d.bin" % (idx))
+        is_exist = os.path.exists(lidar_filename)
+        if is_exist:
+            return utils.load_velo_scan(lidar_filename), is_exist
+        else:
+            return None, is_exist
+        # print(lidar_filename, is_exist)
+        # return utils.load_velo_scan(lidar_filename), is_exist
+
+    def get_top_down(self, idx):
+        pass
+
+    def isexist_pred_objects(self, idx):
+        assert idx < self.num_samples and self.split == "training"
+        pred_filename = os.path.join(self.pred_dir, "%06d.txt" % (idx))
+        return os.path.exists(pred_filename)
+
+    def isexist_depth(self, idx):
+        assert idx < self.num_samples and self.split == "training"
+        depth_filename = os.path.join(self.depth_dir, "%06d.txt" % (idx))
+        return os.path.exists(depth_filename)
+
+
+def save_image_with_boxes(img, objects, calib, path_to_save):
+    """ Show image with 2D bounding boxes """
+    if objects is None:
+        print('no objects')
+        return 0
+
+    img1 = np.copy(img)  # for 3d bbox
+    for obj in objects:
+        if obj.type == "DontCare":
+            continue
+        box3d_pts_2d, box3d_pts_3d = utils.compute_box_3d(obj, calib.P)
+        if box3d_pts_2d is None:
+            continue
+        img1 = utils.draw_projected_box3d(img1, box3d_pts_2d)
+
+    cv2.imwrite(path_to_save, img1)
+
+
+if __name__ == "__main__":
+    import mayavi.mlab as mlab
+
+    parser = argparse.ArgumentParser(description="My KIITI Object Visualization")
+    parser.add_argument(
+        "--rootdir",
+        type=str,
+        default="",
+        metavar="N",
+        help="no help",
+    )
+    parser.add_argument(
+        "--savedir",
+        type=str,
+        default="",
+        metavar="N",
+        help="no help",
+    )
+    parser.add_argument(
+        "--preds_path",
+        type=str,
+        default="",
+        help="no help",
+    )
+
+    # Default arguments
+    parser.add_argument(
+        "-d",
+        "--dir",
+        type=str,
+        default="data/object",
+        metavar="N",
+        help="input  (default: data/object)",
+    )
+    parser.add_argument(
+        "-i",
+        "--ind",
+        type=int,
+        default=0,
+        metavar="N",
+        help="input  (default: data/object)",
+    )
+    parser.add_argument(
+        "-p", "--pred", action="store_true", help="show predict results"
+    )
+    parser.add_argument(
+        "-s",
+        "--stat",
+        action="store_true",
+        help=" stat the w/h/l of point cloud in gt bbox",
+    )
+    parser.add_argument(
+        "--split",
+        type=str,
+        default="training",
+        help="use training split or testing split (default: training)",
+    )
+    parser.add_argument(
+        "-l",
+        "--lidar",
+        type=str,
+        default="velodyne",
+        metavar="N",
+        help="velodyne dir  (default: velodyne)",
+    )
+    parser.add_argument(
+        "-e",
+        "--depthdir",
+        type=str,
+        default="depth",
+        metavar="N",
+        help="depth dir  (default: depth)",
+    )
+    parser.add_argument(
+        "-r",
+        "--preddir",
+        type=str,
+        default="pred",
+        metavar="N",
+        help="predicted boxes  (default: pred)",
+    )
+    parser.add_argument("--gen_depth", action="store_true", help="generate depth")
+    parser.add_argument("--vis", action="store_true", help="show images")
+    parser.add_argument("--depth", action="store_true", help="load depth")
+    parser.add_argument("--img_fov", action="store_true", help="front view mapping")
+    parser.add_argument("--const_box", action="store_true", help="constraint box")
+    parser.add_argument(
+        "--save_depth", action="store_true", help="save depth into file"
+    )
+    parser.add_argument(
+        "--pc_label", action="store_true", help="5-verctor lidar, pc with label"
+    )
+    parser.add_argument(
+        "--dtype64", action="store_true", help="for float64 datatype, default float64"
+    )
+
+    parser.add_argument(
+        "--show_lidar_on_image", action="store_true", help="project lidar on image"
+    )
+    parser.add_argument(
+        "--show_lidar_with_depth",
+        action="store_true",
+        help="--show_lidar, depth is supported",
+    )
+    parser.add_argument(
+        "--show_image_with_boxes", action="store_true", help="show lidar"
+    )
+    parser.add_argument(
+        "--show_lidar_topview_with_boxes",
+        action="store_true",
+        help="show lidar topview",
+    )
+
+    args = parser.parse_args()
+    dataset = kitti_object(args.rootdir, split=args.split, args=args)
+    for data_idx in range(len(dataset)):
+        img = dataset.get_image(data_idx)
+        calib = dataset.get_calibration(data_idx)
+        objects = dataset.get_pred_objects(data_idx)
+
+        save_path = args.savedir + '/' + str(data_idx).rjust(6, '0') + '.jpg'
+        save_image_with_boxes(img, objects, calib, path_to_save=save_path)
